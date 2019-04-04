5eec346e0d8669bd897c4f5a1f2c59d2453af7e1
JizongL <2015rpro@gmail.com>
2019-04-04 11:22:06 -0400

    all setup

--- app.js
+++
@@ -1,7 +0,0 @@
-app.use(helmet())
-
-app.get('/',(req,res)=>{
-  res.send('Hello,world!')
-})
-
-module.exports = app
--- src/app.js
+++ src/app.js
@@ -3,10 +3,10 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-
+const {NODE_ENV} = require('./config')
 const app = express()
 
-const morganOption = (process.env.NODE_ENV === 'production')
+const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
@@ -14,7 +14,16 @@ app.use(morgan(morganOption))
 app.use(cors())
 app.use(helmet())
 
-
+app.use(function errorHandler(error, req, res, next) {
+    let response
+    if (NODE_ENV === 'production') {
+      response = { error: { message: 'server error' } }
+    } else {
+      console.error(error)
+      response = { message: error.message, error }
+    }
+    res.status(500).json(response)
+  })
 
 app.get('/',(req,res)=>{
   res.send('Hello, world!')
