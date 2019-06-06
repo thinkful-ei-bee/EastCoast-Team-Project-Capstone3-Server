8d7231520e86b4a5ff3984a7a656cc4213f9fa23
JizongL <2015rpro@gmail.com>
2019-06-06 15:59:44 -0400

    first commit

---
+++ migrations/001.do.create_users.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "users" (
+    "id" SERIAL PRIMARY KEY,
+    "full_name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "gender" TEXT NOT NULL,
+    "date_created" TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
+);
+
+
