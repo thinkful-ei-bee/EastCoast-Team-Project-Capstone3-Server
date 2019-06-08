24538469adf2614c380b1003cc51a7aa3bca204a
JizongL <2015rpro@gmail.com>
2019-06-07 22:38:19 -0400

    setting up server

--- src/config.js
+++ src/config.js
@@ -4,5 +4,5 @@ module.exports = {
   DB_URL: process.env.DB_URL
     || 'postgresql://dunder-mifflin@localhost/rendezvous',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-  
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 }
