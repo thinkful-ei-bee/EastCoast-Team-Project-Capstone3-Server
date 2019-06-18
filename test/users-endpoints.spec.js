747ea5033741cc9a8ee3d9e03fd2881901eef4a5
LaurelButler <laurelbutler126@gmail.com>
2019-06-18 12:50:18 -0400

    wrote required fields test for users

--- test/users-endpoints.spec.js
+++ test/users-endpoints.spec.js
@@ -11,4 +11,35 @@ describe('User Endpoints', function(){
         db = helpers.makeKnexInstance()
         app.set('db'. db)
     })
+
+    after('disconnect from db', () => db.destroy())
+
+    before('cleanup', () => helpers.cleanTables(db))
+
+    afterEach('cleanup', () => helpers.cleanTables(db))
+
+    describe(`POST /api/users`, () => {
+        this.beforeEach('insert users', () => helpers.seedUsers(db, testUsers))
+
+        const requiredFields = ['email', 'gender', 'full_name', 'password']
+        requiredFields.forEach(field => {
+            const registerAttemptBody = {
+                email: 'test email',
+                gender: 'test gender',
+                full_name: 'test full_name',
+                password: 'test password'
+            }
+            it(`responds with 400 required error when '${field}' is missing`, () => {
+              delete registerAttemptBody[field]
+              
+              return supertest(app)
+                .post('/api/users')
+                .send(registerAttemptBody)
+                .expect(400, {
+                    error: `Missing '${field}' in request body`,
+                })
+            })
+        })
+        
+    })
 })
