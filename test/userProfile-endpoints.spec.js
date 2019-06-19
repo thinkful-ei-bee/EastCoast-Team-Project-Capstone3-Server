c23a4c236358c23b9bbc7e895ec6ef0370337a7b
LaurelButler <laurelbutler126@gmail.com>
2019-06-19 12:38:47 -0400

    working on user profile testing

--- test/userProfile-endpoints.spec.js
+++ test/userProfile-endpoints.spec.js
@@ -2,8 +2,13 @@ const jwt = require('jsonwebtoken')
 const app = require('../src/app')
 const helpers = require('./test-helpers')
 
-describe.skip('UserProfile Endpoints', function () {
+describe.only('UserProfile Endpoints', function () {
   let db
+
+  const {
+    testUsers,
+    testProfiles
+  } = helpers.makeProfileFixtures();
 
   before('make knex instance', () => {
     db = helpers.makeKnexInstance()
@@ -16,11 +21,29 @@ describe.skip('UserProfile Endpoints', function () {
 
   afterEach('cleanup', () => helpers.cleanTables(db))
 
-  describe(`GET /user_profile`, () => {
+  describe(`GET /user_profile/current-user`, () => {
     beforeEach('insert user_profile', () => {
-      /* implement me */
+      return helpers.seedUserProfiles(
+        db,
+        testUsers,
+        testProfiles
+      )
     })
-      
+    it('should respond to GET `/current-user` with an array of profiles and a status 200', () => {
+      return supertest(app)
+        .get('/api/user_profile/current-user')
+        .set('Authorization', helpers.makeAuthHeader(testUsers[0]))
+        .expect(200)
+        .expect(res => {
+          expect(res.body).to.be.a('array')
+          res.body.forEach(profile => {
+            expect(profile).to.be.a('object')
+            expect(profile).to.include.keys('id', 'profile_picture', 'music_like', 'movie_like', 'me_intro', 'user_id')
+          })
+        })
+    }) 
 })
+
+describe()
 
 })
