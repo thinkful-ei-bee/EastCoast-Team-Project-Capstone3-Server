b03492104d1838369f46b0ed48be1b5e6c8cca2f
JizongL <2015rpro@gmail.com>
2019-06-20 14:47:53 -0400

    fixed client origin import

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
-const { NODE_ENV } = require('./config')
+
+const { NODE_ENV,CLIENT_ORIGIN  } = require('./config')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
