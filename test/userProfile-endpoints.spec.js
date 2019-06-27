9ee3e992d2ac2905d1e9a5f37c1759cf3d81db8a
Farah Student <farah.gus@hotmail.com>
2019-06-27 09:35:12 -0400

    app.spec

--- test/userProfile-endpoints.spec.js
+++ test/userProfile-endpoints.spec.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 const app = require('../src/app')
 const helpers = require('./test-helpers')
 
-describe.only('UserProfile Endpoints', function () {
+describe('UserProfile Endpoints', function () {
   let db
 
   const {
